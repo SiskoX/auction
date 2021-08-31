187aca19ca3879422a6788fd548bc1a40481085f
SiskoX <adamsfey3@gmail.com>
2021-08-31 20:57:59 +0300

    productmodel

---
+++ models/products.py
@@ -0,0 +1,22 @@
+
+
+class Product :
+    id = 
+    name = 
+    year = 
+    created_at = 
+    description= 
+    
+
+class Catagories():
+    electronics :
+    collectable :
+    cyptocurrency:
+    shoes: 
+    art:
+    furniture:
+    sculpture:
+    car_parts:
+
+
+
