958b1b86ba3ce075d34c1799d4e2b4600dc982e0
SiskoX <adamsfey3@gmail.com>
2021-08-31 20:56:20 +0300

    order

---
+++ models/order.py
@@ -0,0 +1,23 @@
+
+
+
+class Order(BaseModel):
+    pass
+
+class Sell (BaseModel):
+    id = 
+    title = String()
+    condition_description = 
+    photo  = 
+    strating_pirce = 
+    buy_it_now_price = 
+    reserve_price = 
+    quantity = 
+    duration = 
+    item_description = 
+    
+
+
+class ShippingAddress (BaseMOdel):
+    address = 
+    phone_number 
